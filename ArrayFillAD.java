c68b1b9c49759fe415bfd3e9746fef8c9177fb4e
AbbieDyck <38113369+AbbieDyck@users.noreply.github.com>
2020-12-09 18:00:35 -0500

    Update ArrayFillAD.java

--- ArrayFillAD.java
+++ ArrayFillAD.java
@@ -1,16 +1,16 @@
-/*************************************************************
-**															**
-** Name:		Abbie Dyck									**
-** Date:		September 17th, 2019						**
-** Class Name:	ArrayFillAD.java							**
+/*****************************************************************
+**								**
+** Name:	Abbie Dyck					**
+** Date:	September 17th, 2019				**
+** Class Name:	ArrayFillAD.java				**
 ** Description:	This program takes in 2 numbers from the 	**
-**				user and will print out the numbers from	**
-**				the lowest number to just before the highest**
-**				number using a for loop. it'll then print 	**
-**				out all of those numbers, with a comma in 	**
-**				between.									**
-**														    **
-**************************************************************/
+**		user and will print out the numbers from	**
+**		the lowest number to just before the highest	**
+**		number using a for loop. it'll then print 	**
+**		out all of those numbers, with a comma in 	**
+**		between.					**
+**								**
+******************************************************************/
 package package1;
 
 import java.util.Scanner;
@@ -23,16 +23,16 @@ public class ArrayFillAD {
 		int numberTwo 		= 0;								//Variable to hold the second number
 		int startingNumber 	= 0;								//Variable to hold the lowest number
 		int endNumber 		= 0;								//Variable to hold the highest number
-		int[] numberArray 	= new int[100];						//Array to hold all of the values
+		int[] numberArray 	= new int[100];							//Array to hold all of the values
 
 
-		Scanner scannerObj = new Scanner(System.in);			//Creates the scanner obj
+		Scanner scannerObj = new Scanner(System.in);						//Creates the scanner obj
 		
-		System.out.println("Please enter the first number: "); 	//Ask user to input the first number
-		numberOne = Integer.parseInt(scannerObj.nextLine());	//Puts that number in numberOne var
+		System.out.println("Please enter the first number: "); 					//Ask user to input the first number
+		numberOne = Integer.parseInt(scannerObj.nextLine());					//Puts that number in numberOne var
 		
-		System.out.println("Please enter the second number: ");	//Ask user to input the second number
-		numberTwo = Integer.parseInt(scannerObj.nextLine());	//Puts that number in numberTwo var
+		System.out.println("Please enter the second number: ");					//Ask user to input the second number
+		numberTwo = Integer.parseInt(scannerObj.nextLine());					//Puts that number in numberTwo var
 		
 		
 		if(numberOne < numberTwo) {								//Test to see what number is the lowest
@@ -43,13 +43,13 @@ public class ArrayFillAD {
 			endNumber = numberOne - 1;							//Sets the highest number to the ending number minus one
 		}
 		
-		for(int x = startingNumber; x <=  endNumber; x++) {		//For loop to put all the number into the array
-			numberArray[x-startingNumber] = x;					//Puts numbers in the array
-			System.out.print(numberArray[x-startingNumber]);	//Prints out all the variables
+		for(int x = startingNumber; x <=  endNumber; x++) {					//For loop to put all the number into the array
+			numberArray[x-startingNumber] = x;						//Puts numbers in the array
+			System.out.print(numberArray[x-startingNumber]);				//Prints out all the variables
 			if(x != endNumber) {								//Test to see if its the last number, and if no, put a comma, but if the last number don't
 				System.out.print(", ");
 			}//End if()
 		}//End for()
-		scannerObj.close();										//Closes the scanner
+		scannerObj.close();									//Closes the scanner
 	}//End main()
 }//End ArrayFillAD()
