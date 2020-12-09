1fdf3989ede6905dd74f7781d9708275e9cc4a8c
AbbieDyck <38113369+AbbieDyck@users.noreply.github.com>
2020-12-09 18:14:46 -0500

    Update TowersOfHanoiLevel4FSEAD.java

--- TowersOfHanoiLevel4FSEAD.java
+++ TowersOfHanoiLevel4FSEAD.java
@@ -1,13 +1,13 @@
 /*************************************************************
-**															**
-** Name:		Abbie Dyck									**
-** Date:		January 7th, 2020							**
-** Class Name:	TowersOfHanoiLevel4FSEAD.java				**
-** Description:	This program takes in the number of disk	**
-**				from the user, then will move them all from **
-**				the left disk to the right disk (It starts  **
-**				playing the Towers of Hanoi game).			**
-**														    **
+**					         	    **
+** Name:	Abbie Dyck				    **
+** Date:	January 7th, 2020			    **
+** Class Name:	TowersOfHanoiLevel4FSEAD.java		    **
+** Description:	This program takes in the number of disk    **
+**		from the user, then will move them all from **
+**		the left disk to the right disk (It starts  **
+**		playing the Towers of Hanoi game).	    **
+**							    **
 **************************************************************/
 package package1;
 
@@ -19,30 +19,30 @@ public class TowersOfHanoiLevel4FSEAD {
 		
 		Scanner scannerObj = new Scanner(System.in);					//ScannerObj to take in the number of disk
 
-		System.out.println("Please enter the number of disk: ");		//Ask the user to enter the number is disk
-		int numberOfDisk = scannerObj.nextInt();						//Makes numberOfDisk equal to what the user entered
-		System.out.println(); 											//Prints a blank line
+		System.out.println("Please enter the number of disk: ");			//Ask the user to enter the number is disk
+		int numberOfDisk = scannerObj.nextInt();					//Makes numberOfDisk equal to what the user entered
+		System.out.println(); 								//Prints a blank line
 		
-		movingTheDisk(numberOfDisk, '1', '3', '2');						//Calls movingTheDisk to play the game
-		scannerObj.close();												//Closes scannerObj
+		movingTheDisk(numberOfDisk, '1', '3', '2');					//Calls movingTheDisk to play the game
+		scannerObj.close();								//Closes scannerObj
 	}//End main()
 
-/*********************************************************************
-** Name:        movingTheDisk()										**
-** Input:       numberOfDisk	- Number of disk the game			**
-**								  is playing with					**
-**				from 			- The peg the move is starting from	**
-**				to 				- The peg the move is going to		**
-**				aux 			- The peg not being used to move	**
-**								  in that turn						**
-** Output:      void								 				**
+/*************************************************************************
+** Name:        movingTheDisk()						**
+** Input:       numberOfDisk	- Number of disk the game		**
+**		                  is playing with			**
+**		from 		- The peg the move is starting from	**
+**		to 		- The peg the move is going to		**
+**		aux 		- The peg not being used to move	**
+**				  in that turn				**
+** Output:      void							**
 ** Description: This method will play the Towers of Hanoi game by 	**
-**				Calling itself recursively.							**
-**********************************************************************/	
+**		Calling itself recursively.				**
+**************************************************************************/	
 	static void movingTheDisk(int numberOfDisk, char from, char to, char aux) {
-		if (numberOfDisk == 1) {										//If numberOfDisk is equal to 1
-			System.out.println("1 to " + to);							//Print the move made
-			return;														//Return
+		if (numberOfDisk == 1) {							//If numberOfDisk is equal to 1
+			System.out.println("1 to " + to);					//Print the move made
+			return;									//Return
 		}//End if()
 		movingTheDisk(numberOfDisk - 1, from, aux, to);					//Calls movingTheDisk to make the next move
 		
